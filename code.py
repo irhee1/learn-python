b697a1fb9768aac4066bf605c219235cc9d3e9b4
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-18 20:59:09 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,3 @@
 import streamlit as s
-s.set_page_config(page_title="Learn python", page_icon="🐍", layout="wide")
-s.header("Learn python")
+s.set_page_config(page_title="Python projects", page_icon="🐍", layout="wide")
+s.header("My python projects")
