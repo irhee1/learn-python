a45457988dd357af7221e827e99254ca52c9c1f1
irhee1 <156379592+irhee1@users.noreply.github.com>
2024-01-19 20:45:58 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
-import streamlit as s
-s.set_page_config(page_title="Python projects", page_icon="🐍", layout="wide")
-s.header("My python projects")
+import streamlit as st
+st.set_page_config(page_title="Python projects", page_icon="🐍", layout="wide")
+st.header("My python projects")
+st.header("Will be avaliable by the summer of 2024!")
